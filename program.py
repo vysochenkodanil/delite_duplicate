bf243597cfbc5b5670107ed12e6ed7e3e7832578
Данил Высоченко <vysochenkodanil@gmail.com>
2024-10-26 13:13:41 +0300

    код

---
+++ program.py
@@ -0,0 +1,14 @@
+def main(a, b):
+    result = list(dict.fromkeys(b))
+    diference = a - len(result)
+    result.extend(diference * '_')
+    
+    return result  
+
+if __name__ == '__main__':
+    a = int(input())
+    b = list(map(int, input().split()))
+
+    
+    result = main(a, b)
+    print(' '.join(map(str, result)))
